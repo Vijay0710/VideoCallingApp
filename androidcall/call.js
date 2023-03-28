5d1698f8916543472b491607bb4b6df12ba61307
Vijay A <vijaymangalani4588@gmail.com>
2023-03-28 07:56:00 +0530

    Added Server

---
+++ androidcall/call.js
@@ -0,0 +1,84 @@
+let localVideo = document.getElementById("local-video")
+let remoteVideo = document.getElementById("remote-video")
+
+localVideo.style.opacity = 0
+remoteVideo.style.opacity = 0
+
+localVideo.onplaying = () => { localVideo.style.opacity = 1 }
+remoteVideo.onplaying = () => { remoteVideo.style.opacity = 1 }
+
+let peer
+
+//peerjs --port 9000 --key peerjs --path /videocallapp  
+function init(userId) {
+    peer = new Peer(userId, {
+        host: '192.168.29.4',
+        // host: 'https://ascendant-epoch-376910.de.r.appspot.com',
+        path: '/videocallapp',
+        port: 9000,
+        
+    })
+
+    listen()
+}
+
+let localStream
+function listen() {
+    peer.on('call', (call) => {
+
+        navigator.getUserMedia({
+            audio: true, 
+            video: true
+        }, (stream) => {
+            localVideo.srcObject = stream
+            localStream = stream
+
+            call.answer(stream)
+            call.on('stream', (remoteStream) => {
+                remoteVideo.srcObject = remoteStream
+
+                remoteVideo.className = "primary-video"
+                localVideo.className = "secondary-video"
+
+            })
+
+        })
+        
+    })
+}
+
+function startCall(otherUserId) {
+    navigator.getUserMedia({
+        audio: true,
+        video: true
+    }, (stream) => {
+
+        localVideo.srcObject = stream
+        localStream = stream
+
+        const call = peer.call(otherUserId, stream)
+        call.on('stream', (remoteStream) => {
+            remoteVideo.srcObject = remoteStream
+
+            remoteVideo.className = "primary-video"
+            localVideo.className = "secondary-video"
+        })
+
+    })
+}
+
+function toggleVideo(b) {
+    if (b == "true") {
+        localStream.getVideoTracks()[0].enabled = true
+    } else {
+        localStream.getVideoTracks()[0].enabled = false
+    }
+} 
+
+function toggleAudio(b) {
+    if (b == "true") {
+        localStream.getAudioTracks()[0].enabled = true
+    } else {
+        localStream.getAudioTracks()[0].enabled = false
+    }
+} 
